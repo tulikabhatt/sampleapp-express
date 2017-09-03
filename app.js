0706d0993f3bda9d43df4d5688074e26ad1d04bd
Tulika Bhatt <tulikabhatt92@gmail.com>
2017-09-03 18:45:21 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
-const express = require('express')
-const app = express()
-_dirname = '/home/strider/github/sampleapp-express'
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
-  //res.send('Hello World!')
-})
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
 
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-app.get('/user', function (req, res) {
-  res.send('Got a GET request at /user')
-})
+var app = express();
 
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
-})
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-  console.log('Directory name '+_dirname);
-})
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
